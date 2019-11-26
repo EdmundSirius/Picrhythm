802946e55f806fce97bcb0fdc4dbca25397e4745
Liu <liu@triumphal.cn>
2019-11-26 22:04:26 +0800

    some work for photos

---
+++ Picrhythm/PhotoStreamViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoStreamViewController.swift
+//  Picrhythm
+//
+//  Created by apple on 2019/11/26.
+//  Copyright © 2019 437.Inc. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class PhotoStreamViewController:UICollectionViewController{
+    
+}
